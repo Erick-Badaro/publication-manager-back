6e64d475987ee57325b464e3b92420b5d3e09025
Erick Badaró <zaterick10@gmail.com>
2025-11-10 12:52:19 -0300

    configurações iniciais de arquivos

---
+++ src/main/java/com/publicationmanager/publication_manager/PublicationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.publicationmanager.publication_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicationManagerApplication.class, args);
+	}
+
+}
